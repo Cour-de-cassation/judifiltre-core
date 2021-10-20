646c710ed6836d9aa97db4114015bcdde129a258
BenoitSerrano <benoit.serrano10@gmail.com>
2021-10-20 15:54:40 +0200

    first commit

---
+++ src/modules/decision/index.ts
@@ -0,0 +1,6 @@
+export type { decisionType };
+
+type decisionType = {
+  _id: number;
+  text: string;
+};
---
+++ src/modules/index.ts
@@ -0,0 +1,6 @@
+import { decisionType } from "./decision";
+import { publicityInfoType, publicityInfoModule } from "./publicityInfo";
+
+export { publicityInfoModule };
+
+export type { decisionType, publicityInfoType };
