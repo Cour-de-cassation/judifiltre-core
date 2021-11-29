790f6fa8f45259d50711b4a14f63393cdeda5f07
BenoitSerrano <benoit.serrano10@gmail.com>
2021-11-29 17:03:59 +0100

    compile

--- dist/modules/publicityInfo/types.d.ts
+++ dist/modules/publicityInfo/types.d.ts
@@ -4,6 +4,7 @@ declare type publicityInfoType = {
     _id: idType;
     sourceId: number;
     sourceDb: "jurinet" | "jurica";
+    publicityRequest: "public" | "nonPublic" | undefined;
     releasabilityStatus: "releasable" | "partiallyReleasable" | "nonReleasable" | "undetermined";
     jurisdiction: "CC" | "CA" | "TJ";
     codeField: string;
