1f3f0a3f1b2d4e1866a402c660ce0329796b152f
antoinefa <fabien.antoine@m4x.org>
2021-12-23 10:44:02 +0100

    update dist

--- dist/modules/publicityInfo/lib/converter.d.ts
+++
@@ -1,9 +0,0 @@
-import { publicityInfoType } from "../types";
-export { converter };
-declare const converter: {
-    convertParameters: typeof convertParameters;
-};
-declare function convertParameters(params: {
-    _id: string;
-    sourceDb: string;
-}): Pick<publicityInfoType, "_id" | "sourceDb"> | undefined;
