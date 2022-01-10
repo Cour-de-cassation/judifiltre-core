76e21bcd5a24ad6635172ce24f1b65fa003de2c7
David Libeau <David.Libeau@justice.fr>
2022-01-10 18:06:45 +0100

    Improve generateDecisions

--- dist/modules/jurinetDecision/lib/generateJurinetDecision.js
+++ dist/modules/jurinetDecision/lib/generateJurinetDecision.js
@@ -4,7 +4,7 @@ exports.generateJurinetDecision = void 0;
 const id_1 = require("../../id");
 const MAX_RAND_NUMBER = 1000000;
 function generateJurinetDecision(decisionFields = {}) {
-    return Object.assign({ _id: id_1.idModule.lib.buildId(), DOCUMENT_ID: Math.floor(Math.random() * MAX_RAND_NUMBER), CAT_PUB: "W", DOCTRINE: "", ID_CHAMBRE: "", ID_SOLUTION: "", IND_ADRESSE: 1, IND_ANO: "0", IND_CADASTRE: 1, IND_CHAINE: 1, IND_COORDONNEE_ELECTRONIQUE: 1, IND_DT_DECE: 1, IND_DT_MARIAGE: 1, IND_DT_NAISSANCE: 1, IND_IMMATRICULATION: 1, IND_NOM_PROFESSIONEL: 0, IND_PRENOM_PROFESSIONEL: 0, JURIDICTION: "Cour d'appel inconnue", NUM_DECISION: "", TEXTE_VISE: "", SOURCE: "", XML: `<?xml version=\"1.0\" ?><DOCUMENT><TEXTE_ARRET>TEXTE DE L'ARRET</TEXTE_ARRET></DOCUMENT>`, TYPE_ARRET: "", RAPROCHEMENT: "", XMLA: "", _analyse: [], _decatt: [], _partie: [], _titrage: [], IND_PM: 0 }, decisionFields);
+    return Object.assign({ _id: id_1.idModule.lib.buildId(), DOCUMENT_ID: Math.floor(Math.random() * MAX_RAND_NUMBER), CAT_PUB: "W", DOCTRINE: "", ID_CHAMBRE: "", ID_SOLUTION: "", IND_ADRESSE: 1, IND_ANO: "0", IND_CADASTRE: 1, IND_CHAINE: 1, IND_COORDONNEE_ELECTRONIQUE: 1, IND_DT_DECE: 1, IND_DT_MARIAGE: 1, IND_DT_NAISSANCE: 1, IND_IMMATRICULATION: 1, IND_NOM_PROFESSIONEL: 0, IND_PRENOM_PROFESSIONEL: 0, JURIDICTION: "Cour d'appel inconnue", NUM_DECISION: "", TEXTE_VISE: "", SOURCE: "", XML: `<?xml version=\"1.0\" ?><DOCUMENT><TEXTE_ARRET>TEXTE DE L'ARRET</TEXTE_ARRET><CAT_PUB>Test</CAT_PUB><LIEN_WWW>https://example.org</LIEN_WWW></DOCUMENT>`, TYPE_ARRET: "", RAPROCHEMENT: "", XMLA: "", _analyse: [], _decatt: [], _partie: [], _titrage: [], IND_PM: 0 }, decisionFields);
 }
 exports.generateJurinetDecision = generateJurinetDecision;
 //# sourceMappingURL=generateJurinetDecision.js.map
